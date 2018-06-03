5a6a93f7a740a29790a9ccb7ee0aad37937b11cd
Kuldeep Verma <kuldeep.b.verma@accenture.com>
2018-06-03 09:11:56 +0530

    config change

--- src/main/java/com/mytaxi/MytaxiAppSwaggerConfig.java
+++ src/main/java/com/mytaxi/MytaxiAppSwaggerConfig.java
@@ -12,6 +12,7 @@ import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * 
@@ -19,6 +20,7 @@ import springfox.documentation.spring.web.plugins.Docket;
  *
  */
 @Configuration
+@EnableSwagger2
 public class MytaxiAppSwaggerConfig
 {
 
